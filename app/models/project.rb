20b9d2eead2f6be7743897c9dda9be30486f44ab
Extazystas <z1m@tut.by>
2013-04-27 16:42:16 +0300

    Build user projects when project creates

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,11 @@ class Project < ActiveRecord::Base
   validates :title, :presence => true
 
   has_many :locale_files
+  belongs_to :user
 
   accepts_nested_attributes_for :locale_files, :allow_destroy => true
+
+  def owner
+    user
+  end
 end
