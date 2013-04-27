4c8763973e703a5b6b449ad10e534b880291fd47
Extazystas <z1m@tut.by>
2013-04-27 20:22:04 +0300

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Retranslator::Application.routes.draw do
   resources :projects
 
-
   devise_for :users
 
   root :to => 'welcome#index'
+
+  match '/dashboard' => 'static_pages#dashboard', :as => :dashboard
 end
