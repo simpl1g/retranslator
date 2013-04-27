29576fead6b8798a4394bd51ee6e0817f7662d41
Konstantin Ilchenko <simpli4eg@gmail.com>
2013-04-27 14:28:13 +0300

    Blank new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retranslator::Application.config.secret_token = '8891f73e3eab46750ae9330b0448ad4990747d03969c6d69b48a8319010976a1c8811a44e2c206c7037c0eb3130d61c9b25b123b367cb621990c29b4fc59ad46'
