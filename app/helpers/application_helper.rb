12644c71d7843143905be04161789758b48cf494
arcadi <arcadi009@gmail.com>
2013-04-28 05:11:21 +0300

    add phrases translations to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def nav_link(link_icon, link_text, link_path)
+  def nav_link(link_icon, link_text, link_path, params={})
 
     is_current_page = url_for(:controller => controller_name, :action => action_name) == link_path
 
     class_name = is_current_page ? 'active' : ''
 
     content_tag(:li, :class => class_name) do
-      link_to link_path do
+      link_to link_path, params do
         content_tag(:i, nil, :class => link_icon) +
         link_text
       end
