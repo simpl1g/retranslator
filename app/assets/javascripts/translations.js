c229c3cab5f2fb040fed8a01dca07c4c9b7f8d3a
Konstantin Ilchenko <simpli4eg@gmail.com>
2013-04-28 14:42:19 +0300

    fix language id passing

--- app/assets/javascripts/translations.js
+++ app/assets/javascripts/translations.js
@@ -1,9 +1,10 @@
 function loadTranslations(element) {
     var translation = $(element);
+    console.log(translation.prev().data('id'))
     $.ajax({
         url:translation.data("url"),
         data: {
-            language: translation.prev().text()
+            language: translation.prev().find('span').data('id')
         }
     }).done(function (data) {
             translation.replaceWith(data);
